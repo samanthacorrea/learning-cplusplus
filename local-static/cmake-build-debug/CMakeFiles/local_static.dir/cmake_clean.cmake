a1f4a4e2848c0aee5c14f680978198688891899b
Samantha Correa <samanthacorrealeite@gmail.com>
2020-12-17 16:32:28 -0400

    local static variable

---
+++ local-static/cmake-build-debug/CMakeFiles/local_static.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/local_static.dir/main.cpp.o"
+  "local_static"
+  "local_static.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/local_static.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
